bffbfeef861d056f036688efd825c112cfcfdc2a
Emeric Bous <38536419+emericb@users.noreply.github.com>
2024-10-21 14:42:27 +0200

    Angular Chuck norris exo
    add type joke
    removed any

--- Angular/exo/src/app/services/chuck-norris.service.ts
+++ Angular/exo/src/app/services/chuck-norris.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Joke} from '../utils/types/joke';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ChuckNorrisService {
 
   constructor(private http: HttpClient) { }
 
-  getRandomJoke() : Observable<any> {
-    return this.http.get<any>(this.API_URL);
+  getRandomJoke() : Observable<Joke> {
+    return this.http.get<Joke>(this.API_URL);
   }
 }
